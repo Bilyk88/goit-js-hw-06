b8b07fcc5d4b93757ddffa8b84bcf4aeb1a1805b
Iryna Bilyk <bilykirochka@gmail.com>
2023-10-04 20:14:45 +0300

    task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const listRef = document.querySelector('#categories');
+
+console.log('Number of categories: ', listRef.children.length);
+
+[...listRef.children].forEach(item => {
+    console.log('Category: ', item.firstElementChild.textContent);
+    console.log('Elements: ', item.lastElementChild.children.length);
+});
