83653de53d31a648b29823b1de307d4537cef706
Iryna Bilyk <bilykirochka@gmail.com>
2023-10-06 14:29:43 +0300

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -9,10 +9,16 @@ const ingredients = [
 
 const ingredientsList = document.querySelector('#ingredients');
 
-ingredients.map(item => {
+const markup = ingredients.map(item => {
   const liItem = document.createElement('li');
   liItem.textContent = item;
   liItem.classList.add('item');
   console.log(liItem);
-  ingredientsList.append(liItem);
+  return liItem;
+  
 });
+
+ingredientsList.append(...markup);
+
+
+
