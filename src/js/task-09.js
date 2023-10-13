065f58708d4df3d7a1e723430ac8b38e68489c69
Iryna Bilyk <bilykirochka@gmail.com>
2023-10-13 21:03:27 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const buttonRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+buttonRef.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  spanRef.textContent = color;
+  bodyRef.style.backgroundColor = color;
+
+}
