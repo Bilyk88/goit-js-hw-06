0a4b413a6ec99ad09335d91e69211600dd1e4956
Iryna Bilyk <bilykirochka@gmail.com>
2023-10-13 20:07:11 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,8 +1,21 @@
-const form = document.querySelector('.login-form');
+const formEl = document.querySelector('.login-form');
 
-form.addEventListener('submit', onSubmit);
+formEl.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
     
+    const { email, password } = event.currentTarget.elements;
+    if (email.value === '' || password.value === '') {
+        return alert('Заповніть всі поля!');
+        
+    } 
+        const data = {
+        email: email.value,
+        password: password.value,
+        }
+    
+    console.log(data);
+    formEl.reset();
+ 
 }
