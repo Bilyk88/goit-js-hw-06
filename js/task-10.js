e3103046f177d0ceb3b6d00670a232621d9ee87f
Iryna Bilyk <bilykirochka@gmail.com>
2023-10-14 21:49:52 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  
+}
+
+function destroyBoxes() {
+  
+}
+
+function createBoxes(amount) {
+  
+}
