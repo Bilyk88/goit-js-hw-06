da8d327e430b025eb89c934a141c1dc692b96e7c
Iryna Bilyk <bilykirochka@gmail.com>
2023-10-16 12:51:45 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -9,17 +9,30 @@ const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
 
+let size = 30;
+
 createBtn.addEventListener('click', createBoxes);
 destroyBtn.addEventListener('click', destroyBoxes);
 
-function createBoxes() {
-  
+function createBoxes(amount) {
+  amount = input.value;
+
+  const markup = [];
+  for (let i = 1; i <= amount; i += 1) {
+    size += 10;
+    const div = document.createElement('div');
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    div.style.backgroundColor = getRandomHexColor();
+    markup.push(div);
+
+  }
+  boxes.append(...markup);
 }
 
 function destroyBoxes() {
-  
+  input.value = '';
+  boxes.innerHTML = '';
+  size = 30;
 }
 
-function createBoxes(amount) {
-  
-}
